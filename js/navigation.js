d94494002d009cf668b1f040f4b8abe657daed24
Giulio Roman <leonixyz@gmail.com>
2016-03-08 10:58:21 +0100

    menu working

--- js/navigation.js
+++ js/navigation.js
@@ -5,7 +5,37 @@
  * navigation support for dropdown menus.
  */
 ( function() {
-	var container, button, menu, links, subMenus, i, len;
+    
+    function toggle(elem) {
+        elem.style.display = (elem.style.display=='none')?'block':'none';
+    }
+
+    function setupChildren(parentLI){
+        if(parentLI.className.indexOf('page_item_has_children')!=-1){
+            parentLI.getElementsByTagName('a')[0].href='#';
+        }
+
+        var childUL = parentLI.getElementsByTagName('ul')[0];
+
+        if(typeof childUL == 'undefined'){
+            return;
+        }
+        else{
+            childUL.style.display='none';
+        }
+
+        parentLI.onclick = function(event) {
+            event.stopPropagation();
+            toggle(childUL);
+        }
+
+        var children = childUL.childNodes;
+        for(var i=0; i<children.length; i++){
+            setupChildren(children[i]);
+        }
+    }
+	
+    var container, button, menu, children;
 
 	container = document.getElementById( 'site-navigation' );
 	if ( ! container ) {
@@ -19,63 +49,10 @@
 
 	menu = container.getElementsByTagName( 'ul' )[0];
 
-	// Hide menu toggle button if menu is empty and return early.
-	if ( 'undefined' === typeof menu ) {
-		button.style.display = 'none';
-		return;
-	}
+    children = menu.getElementsByTagName('li');
 
-	menu.setAttribute( 'aria-expanded', 'false' );
-	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
-		menu.className += ' nav-menu';
-	}
+    for(var i=0; i<children.length; i++){
+        setupChildren(children[i]);
+    }
 
-	button.onclick = function() {
-		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
-			container.className = container.className.replace( ' toggled', '' );
-			button.setAttribute( 'aria-expanded', 'false' );
-			menu.setAttribute( 'aria-expanded', 'false' );
-		} else {
-			container.className += ' toggled';
-			button.setAttribute( 'aria-expanded', 'true' );
-			menu.setAttribute( 'aria-expanded', 'true' );
-		}
-	};
-
-	// Get all the link elements within the menu.
-	links    = menu.getElementsByTagName( 'a' );
-	subMenus = menu.getElementsByTagName( 'ul' );
-
-	// Set menu items with submenus to aria-haspopup="true".
-	for ( i = 0, len = subMenus.length; i < len; i++ ) {
-		subMenus[i].parentNode.setAttribute( 'aria-haspopup', 'true' );
-	}
-
-	// Each time a menu link is focused or blurred, toggle focus.
-	for ( i = 0, len = links.length; i < len; i++ ) {
-		links[i].addEventListener( 'focus', toggleFocus, true );
-		links[i].addEventListener( 'blur', toggleFocus, true );
-	}
-
-	/**
-	 * Sets or removes .focus class on an element.
-	 */
-	function toggleFocus() {
-		var self = this;
-
-		// Move up through the ancestors of the current link until we hit .nav-menu.
-		while ( -1 === self.className.indexOf( 'nav-menu' ) ) {
-
-			// On li elements toggle the class .focus.
-			if ( 'li' === self.tagName.toLowerCase() ) {
-				if ( -1 !== self.className.indexOf( 'focus' ) ) {
-					self.className = self.className.replace( ' focus', '' );
-				} else {
-					self.className += ' focus';
-				}
-			}
-
-			self = self.parentElement;
-		}
-	}
 } )();
