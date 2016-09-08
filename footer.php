26a0fff56a0d2a554842088161a37cc750422370
Giulio Roman <leonixyz@gmail.com>
2016-09-08 10:31:42 +0200

    fix widget area covering comments in mobile mode, fix unclosing div

--- footer.php
+++ footer.php
@@ -13,49 +13,50 @@
  */
 
 ?>
-			</div>
-		</div>
-	</div><!-- .site-content -->
-
-	<div id="pittogramma" style="background-image:url('<?php echo get_option( 'my_theme_settings' )['pictogram']; ?>');"></div>
-
-	<footer id="colophon" class="site-footer unibrand" role="contentinfo">
-		<div class="site-info container">
-			<?php
-				// extract the map URL out of the HTML code provided by Google Maps "embed map" feature
-				$mapHTML = get_option( 'my_theme_settings' )['google_map'];
-				preg_match('/src="[^"]+"/i', $mapHTML, $mapURL);
-
-				if (!empty($mapURL)):
-			?>
-			<div class="row">
-					<div class="col-md-5">
-						<strong><?php echo get_option( 'my_theme_settings' )['google_map_title'];?></strong>
-						<address><?php echo get_option( 'my_theme_settings' )['google_map_address'];?></address>
 					</div>
-					<div class="col-md-7">
-						<iframe id="footer-map" <?php echo $mapURL[0]; ?>></iframe>
-					</div>
-			</div>
-			<?php
-				endif;
-			?>
-			<div class="row" style="margin-top:50px;">
-				<div class="col-xs-12">
-					<a href="<?php bloginfo('template_directory');?>/disclaimer.html">About this site</a>
 				</div>
-			</div>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-	<script>
-		/**
-		 *	Hide the widget area if there are no widgets by expanding the primary div
-		 *
-		 */
-		 var widgetArea = document.getElementsByClassName('widget-area');
-		 if (widgetArea.length == 0) {
-		 	$('#primary').removeClass('col-md-9');
-		 }
+			</div><!-- .site-content -->
+
+			<div id="pittogramma" style="background-image:url('<?php echo get_option( 'my_theme_settings' )['pictogram']; ?>');"></div>
+
+			<footer id="colophon" class="site-footer unibrand" role="contentinfo">
+				<div class="site-info container">
+					<?php
+						// extract the map URL out of the HTML code provided by Google Maps "embed map" feature
+						$mapHTML = get_option( 'my_theme_settings' )['google_map'];
+						preg_match('/src="[^"]+"/i', $mapHTML, $mapURL);
+
+						if (!empty($mapURL)):
+					?>
+					<div class="row">
+							<div class="col-md-5">
+								<strong><?php echo get_option( 'my_theme_settings' )['google_map_title'];?></strong>
+								<address><?php echo get_option( 'my_theme_settings' )['google_map_address'];?></address>
+							</div>
+							<div class="col-md-7">
+								<iframe id="footer-map" <?php echo $mapURL[0]; ?>></iframe>
+							</div>
+					</div>
+					<?php
+						endif;
+					?>
+					<div class="row" style="margin-top:50px;">
+						<div class="col-xs-12">
+							<a href="<?php bloginfo('template_directory');?>/disclaimer.html">About this site</a>
+						</div>
+					</div>
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div><!-- .site -->
+		<script>
+			/**
+			 *	Hide the widget area if there are no widgets by expanding the primary div
+			 *
+			 */
+			 var widgetArea = document.getElementsByClassName('widget-area');
+			 if (widgetArea.length == 0) {
+			 	$('#primary').removeClass('col-md-9');
+			 }
 		</script>
 		<?php wp_footer(); ?>
 	</body>
