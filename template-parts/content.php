d07a2696612f7039348ab50d56bcafa54facdaed
Giulio Roman <leonixyz@gmail.com>
2016-09-09 13:54:37 +0200

    restyle blog posts

--- template-parts/content.php
+++ template-parts/content.php
@@ -12,66 +12,48 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="row">
+<article class="row post" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<header class="entry-header col-xs-12">
+		<div class="entry-meta">
+			<?php unibz_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		
 		<?php
-			if(has_post_thumbnail() && !is_single()) :
+			if ( is_single() ) {
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			} else {
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			}
 		?>
-		<div class="col-sm-4">
-				<div class="entry-meta">
-					<?php unibz_posted_on(); ?>
-				</div><!-- .entry-meta -->
-			<a class="post-thumbnail" href="<?php echo esc_url(get_permalink()) ?>" aria-hidden="true"><img src="<?php the_post_thumbnail_url(); ?>" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" style="width:100%; height:100%"></a>
-		</div>
+	</header><!-- .entry-header -->
 
-		<div class="col-sm-8">
-			<?php
-				else:
-			?>
-			<div class='col-xs-12'>
-				<div class="entry-meta entry-meta-nopicture">
-					<?php unibz_posted_on(); ?>
-				</div><!-- .entry-meta -->
-				<?php
-					endif;
-				?>
+	<?php
+		if(has_post_thumbnail() && !is_single()) :
+	?>
+	<div class="col-sm-4">
+		<a class="post-thumbnail" href="<?php echo esc_url(get_permalink()) ?>" aria-hidden="true"><img src="<?php the_post_thumbnail_url(); ?>" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" style="width:100%; height:100%"></a>
+	</div>
 
-				<header class="entry-header">
-					<?php
-						if ( 'post' === get_post_type() ) :
-					?>
+	<div class="entry-content col-sm-8">
+	<?php
+		else:
+	?>
+	<div class="entry-content col-xs-12">
+	<?php
+		endif;
+	?>
+		<?php 
+			if ( is_single() ) {
+				the_content();
+			}
+			else {
+				the_excerpt();
+			}
+		?>
+		<footer class="entry-footer">
+			<?php unibz_entry_footer(); ?>
+		</footer><!-- .entry-footer -->
+	</div><!-- .entry-content -->
 
-					<?php
-						endif;
-
-						if ( is_single() ) {
-							the_title( '<h1 class="entry-title">', '</h1>' );
-						} else {
-							the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-						}
-
-					?>
-				</header><!-- .entry-header -->
-
-				<div class="entry-content">
-					<?php 
-						if ( is_single() ) {
-							the_content();
-						}
-						else {
-
-							// @TODO decide whether to link the excerpt with the single page?
-							//echo "<a href='" . esc_url( get_permalink() ) ."'>";
-							the_excerpt();
-							//echo "</a>";
-						}
-
-					?>
-				</div><!-- .entry-content -->
-
-				<footer class="entry-footer">
-					<?php unibz_entry_footer(); ?>
-				</footer><!-- .entry-footer -->
-			</div>
-		</div>
 </article><!-- #post-## -->
