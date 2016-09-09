a1c920b52f40c512837eccbdbfb424ea5adeef60
Giulio Roman <leonixyz@gmail.com>
2016-09-09 14:32:21 +0200

    restyle links, force date to 'd M Y' format

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -17,24 +17,18 @@ function unibz_posted_on() {
 		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 	}
 
+	// force the date to use format (eg. 09 SEP 2016)
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
+		esc_html( get_the_date('d M Y') ),
 		esc_attr( get_the_modified_date( 'c' ) ),
-		esc_html( get_the_modified_date() )
+		esc_html( get_the_modified_date('d M Y') )
 	);
 
-	//'Posted on %s',
 	$posted_on = sprintf(
 		esc_html_x( '%s', 'post date', 'unibz' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		$time_string
 	);
-
-	//remuve the author name
-	/*$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'unibz' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);*/ 
 
 	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; 
 	// WPCS: XSS OK.
