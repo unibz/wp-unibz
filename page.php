986f2e65a11cd92f6049b230eb8ce28815103d20
leonixyz <leonixyz@gmail.com>
2016-12-14 23:32:52 +0100

    remove sidebar if not active

--- page.php
+++ page.php
@@ -17,7 +17,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area col-md-8">
+	<div id="primary" class="content-area <?php if(is_active_sidebar('sidebar-1')) echo 'col-md-8'; ?>">
 		<main id="main" class="site-main" role="main">
 
 			<?php
