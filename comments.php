4a4d27ab24a00cb3a6eded7c0cd99c2ea698997b
leonixyz <leonixyz@gmail.com>
2016-12-16 11:42:30 +0100

    restyle comment form

--- comments.php
+++ comments.php
@@ -93,6 +93,27 @@ if ( post_password_required() ) {
 		array(
 			'class_submit' => 'btn btn-button unibrand',
 			'comment_field' => '<p class="comment-form-comment"><textarea id="comment" name="comment" aria-required="true" class="form-control"></textarea></p>',
+			'fields' => apply_filters('comment_form_default_fields', 
+				$fields =  array(
+
+				  'author' =>
+				    '<p class="comment-form-author"><label for="author">' . __( 'Name', 'domainreference' ) . '</label> ' .
+				    ( $req ? '<span class="required">*</span>' : '' ) .
+				    '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
+				    '" size="30"' . $aria_req . ' class="form-control" /></p>',
+
+				  'email' =>
+				    '<p class="comment-form-email"><label for="email">' . __( 'Email', 'domainreference' ) . '</label> ' .
+				    ( $req ? '<span class="required">*</span>' : '' ) .
+				    '<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+				    '" size="30"' . $aria_req . ' class="form-control" /></p>',
+
+				  'url' =>
+				    '<p class="comment-form-url"><label for="url">' . __( 'Website', 'domainreference' ) . '</label>' .
+				    '<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+				    '" size="30" class="form-control" /></p>',
+				)
+			)
 		)
 	);
 	?>
