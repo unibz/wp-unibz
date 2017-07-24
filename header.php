b7ae4717464246e3a11cd33862c61d0a29f38c2a
leonixyz <leonixyz@gmail.com>
2017-07-24 15:33:12 +0200

    disable compatibility mode from IE

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<link rel='shortcut icon' href='<?php echo get_template_directory_uri() . '/img/favicon.ico'; ?>' type='image/x-icon'/ >
